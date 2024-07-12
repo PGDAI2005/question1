c5ec760c7c1fee89e90b7769f630c673881c185a
Manasi <pgdai2005@gmail.com>
2024-07-12 09:25:04 +0530

    Q1 commit

---
+++ Q1.py
@@ -0,0 +1,12 @@
+# Prompt the user to enter the first number and convert it to an integer
+n1 = int(input("Enter the First Number: "))
+
+# Prompt the user to enter the second number and convert it to an integer
+n2 = int(input("Enter the Second Number: "))
+
+# Calculate the sum of n1 and n2
+sum_result = n1 + n2
+
+# Print the result
+print(f"The sum of {n1} and {n2} is {sum_result}")
+
